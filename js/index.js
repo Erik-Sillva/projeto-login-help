ae1e58a7f0a01af3cf0589f4b4a95be703068bbc
Erik Silva <104636199+Erik-Sillva@users.noreply.github.com>
2022-10-01 18:09:16 -0300

    Add responsividade e js

---
+++ js/index.js
@@ -0,0 +1,25 @@
+const eye = document.getElementById('eye');
+const eyeSlash = document.getElementById('eye-slash');
+const email = document.getElementById('iemail');
+const fieldPassword = document.getElementById('field-password');
+
+function showPassword() {
+
+    if (eye.style.display === 'none') {
+        eye.style.display = 'block'
+        eyeSlash.style.display = 'none'
+        fieldPassword.type = 'text'
+    } else {
+        eye.style.display = 'none'
+        eyeSlash.style.display = 'block'
+        fieldPassword.type = 'password'
+    }
+}
+
+function verificar() {
+    if (email.value === '' || fieldPassword.value === '') {
+        alert('[ERRO] Prencha os campos antes de continuar!');
+    } else {
+        alert('Você está logado!');
+    }
+}
